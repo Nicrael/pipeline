bafabd5240dcd11f403c375436e6c3f722aa8a54
Davide Ricci <davide.ricci82@gmail.com>
2020-04-29 15:52:36 +0200

    Again on formatting.

--- fill_header.py
+++ fill_header.py
@@ -46,9 +46,6 @@ class observatory():
         # No header by default
         self.head = fits.PrimaryHDU().header
 
-        if "JD" not in self.head:
-            self.head['JD'] = Time.now().jd
-
 
     def test(self, filename):
         '''
@@ -68,8 +65,7 @@ class observatory():
     @filename.setter # On new file, update data
     def filename(self, value):
         self._filename = value
-        self.head = r.get_fits_header(value, fast=True)
-
+        self.head = r.get_fits_header(value)
 
     def skycoord(self):
         head = self.head
@@ -80,6 +76,9 @@ class observatory():
                                  height=self.alt)
 
         # time
+        if self.obstime not in self.head:
+            self.head['JD'] = Time.now().jd
+
         timekey = head[self.obstime]
         if 'MJD' in self.obstime:
             obstime = Time(timekey, format='mjd')
@@ -98,9 +97,10 @@ class observatory():
         if self.ra in head and self.dec in head:
             coord = SkyCoord(ra=head[self.ra],
                              dec=head[self.dec],
+                             frame='fk5',
                              unit=self.unit)
         elif self.obj in head:
-            coord = SkyCoord.from_name(head[self.obj])
+            coord = SkyCoord.from_name(head[self.obj], frame='fk5')
         else:
             print("No (RA DEC) and no OBJECT in header")
             return
@@ -127,8 +127,8 @@ class observatory():
         head = self.head
 
         if self.binning and self.binning[0] in head:
-            bins = [self.head[self.binning[0]],
-                    self.head[self.binning[1]]]
+            bins = [head[self.binning[0]],
+                    head[self.binning[1]]]
         else:
             bins = [1, 1]
 
@@ -162,7 +162,7 @@ class observatory():
         cd = self.plate*np.array([[np.cos(angle), np.sin(angle)*flip],
                                   [np.sin(angle), np.cos(angle)]])
 
-        w = WCS()
+        w = WCS(head)
         w.wcs.ctype = ["RA---TAN", "DEC--TAN"]
         w.wcs.cd = cd
         w.wcs.crval = [self.coord.ra.deg,
@@ -176,7 +176,8 @@ class observatory():
 
 
     def newhead(self):
-        nh = fits.PrimaryHDU().header
+
+        nh = self.head
 
         if not hasattr(self, 'plate'):
             self.detector()
@@ -191,24 +192,33 @@ class observatory():
         nh["latitude"] = c.location.lat.deg
         nh["altitude"] = int(c.location.height.to_value())
 
-        # # location (hierarch test)
-        # nh["tel geolon"] = c.location.lon.deg
-        # nh["tel geolat"] = c.location.lat.deg
-        # nh["tel geoelev"] = int(c.location.height.to_value())
+        # location (hierarch test)
+        nh["tel geolon"] = c.location.lon.deg
+        nh["tel geolat"] = c.location.lat.deg
+        nh["tel geoelev"] = int(c.location.height.to_value())
 
         # obstime
         nh["mjd-obs"] = c.obstime.mjd
         nh["jd"] = c.obstime.jd
         nh["date-obs"] = c.obstime.isot[:-4]
+
+        midnight = c.obstime.iso.split()[0]
+
+        nh["utc"] = c.obstime.unix - Time(midnight).unix
         nh["lst"] = c.obstime.sidereal_time("mean").hour
-        nh["equinox"] = c.obstime.jyear
+
+        nh["equinox"] = c.equinox.jyear # should be 2000 for fk5
 
         # detector
         nh["plate"] = self.plate
 
+        if self.obj in nh:
+            nh[self.obj] = nh[self.obj]
+
         # coord
         #nh["ra"] = c.ra.to_string(unit="hourangle",sep=":")
         #nh["dec"] = c.dec.to_string(sep=":")
+        nh["radecsys"] = c.frame.name
         nh["ra"] = c.ra.deg
         nh["dec"] = c.dec.deg
 
@@ -228,8 +238,7 @@ class observatory():
         nh["moondist"] = moon_radec.separation(c).deg
 
         if not hasattr(self, 'w'):
-            if "NAXIS1" in self.head:
-                self.head["NAXIS1"]
+            if "NAXIS1" in nh:
                 self.wcs()
                 nh.extend(self.w.to_header(), update=True)
 
@@ -238,12 +247,12 @@ class observatory():
 
         nh.add_history("Created by "+self.newhead.__qualname__)
 
-        nh = sethead(nh)
+        nh = sethead2(nh)
 
         self.nh = nh
 
 
-def sethead(head):
+def sethead2(head):
     ''' By Davide Ricci.
     Add a keyword in the header with comment and format
     taken from the json config file
@@ -251,31 +260,65 @@ def sethead(head):
     Out[51]: 'Hello, 123.13'
     '''
 
-    with open('cerbero-merged-dict.json') as jfile:
+    with open('cerbero-merged-test.json') as jfile:
         header_dict = json.load(jfile)# ['primary']
 
-    #card = [fits.Card(**c) for c in head_dict if key == c['keyword'] ][0]
-    #form = '' if not card else card[0]["format"]
+    nh = fits.Header([tuple(d) for d in header_dict if d[0] in head ] )
 
-    for key in head :
-        key =  key.lower()
-        if key in header_dict: # keyword in the dictionary
-            val = head[key]
-            form = header_dict[key][0]  # Format in the dictionary
-            comm =  header_dict[key][1] # Comment in the dictionary
-            if 'd' in form:
-                value = int(round(float(val)))
-            elif 'f' in form:
-                decimals = [ int(v) for v in form if v.isdigit() ]
-                value = round(val, decimals[0])
-            else: # 's' in form:
-                value = f'{val:{form}}'
-            print(key, val, "→", form, "→", value)
-            head[key] = (value, comm)
-        else:
-            print(f'{key} not in dict')
+    for n in nh:
+        form = nh[n]
+        val = head[n]
+        #comm = nh.get_comment(n)
+        if 'd' in form:
+            value = int(round(float(val)))
+        elif 'f' in form:
+            decimals = [ int(v) for v in form if v.isdigit() ]
+            decimals = [0] if not decimals else decimals
+            value = round(val, decimals[0])
+        elif 'b' in form:
+            value = bool(val)
+        else: # 's' in form:
+            value = f'{val:{form}}'
+        #print(n, val, "→", form, "→", value)
+        nh[n] = (value, )
 
-    return head
+    return nh
+
+
+# def sethead(head):
+#     ''' By Davide Ricci.
+#     Add a keyword in the header with comment and format
+#     taken from the json config file
+#     In [47]: f'Hello, {123.129:.2f}'
+#     Out[51]: 'Hello, 123.13'
+#     '''
+
+#     with open('cerbero-merged-dict.json') as jfile:
+#         header_dict = json.load(jfile)# ['primary']
+
+#     #card = [fits.Card(**c) for c in head_dict if key == c['keyword'] ][0]
+#     #form = '' if not card else card[0]["format"]
+
+#     for key in head :
+#         key =  key.lower()
+#         if key in header_dict: # keyword in the dictionary
+#             val = head[key]
+#             form = header_dict[key][0]  # Format in the dictionary
+#             comm =  header_dict[key][1] # Comment in the dictionary
+#             if 'd' in form:
+#                 value = int(round(float(val)))
+#             elif 'f' in form:
+#                 decimals = [ int(v) for v in form if v.isdigit() ]
+#                 value = round(val, decimals[0])
+#             else: # 's' in form:
+#                 value = f'{val:{form}}'
+#             print(key, val, "→", form, "→", value)
+#             head[key] = (value, comm)
+#         else:
+#             print(f'{key} not in dict')
+
+#     return head
+
 
 
 def main():
